e51cf646772fd3e979fbb366344a79ca49d97211
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2018-03-21 03:00:17 +0100

    Initial commit: README and benchmark generator

---
+++ README.md
@@ -0,0 +1,33 @@
+# Go vs PHP: a benchmark in a real business case.
+> Aiming to benchmark the difference in performance between Go and PHP given a
+> common problem to solve and using similar algorithms.
+
+## The problem 
+The problem aims to emulate an existing real business case which has been
+abstracted into a simple sorting problem.
+
+Given a list of tuples with values `(x, y, z)`, sort them in a matrix
+structure like:
+
+```
+   |  x1 |  x2 |  x3 |
+y1 | z11 | z21 | z31 |
+y2 | z12 | z22 | z32 |
+y3 | z13 | z23 | z33 |
+```
+The end result is a valid CSV output.
+
+In order to simulate the business case, the data has to be formated in three different ways:
+
+1. `[(x1, y1, z11), (x2, y2, z22), (x3, y3, z33)]`
+2. `[x1 => [y1 => z11, y2 => z12], x2 => [y1 => z21, y2 => z22]]`
+3. `[ [#, x1, x2, x3], [y1, z11, z21, z31], [y2, z12, z22, z32], [y3, z13, z23, z33] ]`
+
+## Requirements
+- Only standard library.
+- The `x` are `string` values while `y` are `integer`. The value of `z` is
+  irrelevant for the case.
+- The `x` values are 200 random words. The `y` values are 500 random numbers. In
+  total, there will be 100000 tuples.
+- The `x` values are already known.
+- The list of tuples is shuffled.
---
+++ generate-data.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Generates a random corpus for the benchmark.\n\n
+
+The output of the script are two files, with the following names by default:
+
+    corpus - Contains the tuples of the corpus. Each line is a different tuple
+    with the format "x y value".
+
+    corpus-x - Contains the x values of the corpus.
+
+In order to generate the words, it uses FreeBSD's dictionary, which can be
+found in /usr/share/dict/words in most of Linux distributions and OSX (Windows
+not supported).
+"""
+from random import shuffle
+from argparse import ArgumentParser
+from argparse import RawTextHelpFormatter
+
+# Dictionary path.
+WORD_FILE = '/usr/share/dict/words'
+
+
+def run(tuples_file, x_file):
+    with open(WORD_FILE) as rwf:
+        all_words = rwf.read().splitlines()
+        shuffle(all_words)
+
+        words = all_words[:200]
+        assert len(words) == 200
+
+        ys = range(0, 500)
+        assert len(ys) == 500
+
+        values = ['%s %d x=%s,y=%d' % (x, y, x, y) for x in words for y in ys]
+        assert len(values) == 100000
+
+        shuffle(values)
+
+        with open(x_file, 'w') as wxf:
+            for word in words:
+                wxf.write('%s\n' % word)
+
+        with open(tuples_file, 'w') as wtf:
+            for value in values:
+                wtf.write('%s\n' % value)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=RawTextHelpFormatter)
+    parser.add_argument('corpus', help='Name of the output file.', nargs='?',
+                        default='corpus')
+    args = parser.parse_args()
+    corpus = args.corpus
+    x = '%s-x' % corpus
+    run(corpus, x)
